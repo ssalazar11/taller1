3131d4873ac6b0bfbd9a50c877c689646fa80325
Samuel Salazar <sa.salazarasalazar@gmail.com>
2022-08-02 09:38:28 -0500

    Cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Sami Salazar'})
